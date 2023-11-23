05341069b0c19a21d8ae4bc244b543b818a45abd
ckorir <chrisruto.k@gmail.com>
2023-11-23 16:19:21 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even_integers = [num for num in num_list if num % 2 == 0]
+    return even_integers
     pass
 
 def make_exclamation(sentence_list):
+    add_exclamation = [str + '!' for str in sentence_list]
+    return add_exclamation
     pass
